d4bd8a34105282c0d356adb7c8150b6db69d61bc
sabirul-islam <sabirul.shimul@gmail.com>
2020-08-07 11:53:36 +0600

    null vs undefined cleared for interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,22 @@
+let shimul;
+
+function add(num1, num2){
+    console.log(num1 + num2);
+    return;
+}
+const result = add(13, 82);
+
+function add(num1, num2){
+    console.log(num1, num2);
+    return;
+}
+const result = add(13);
+
+const person = {name:'shimul', phone: '01911859175'}
+console.log(person.address);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2, 5, 9];
+console.log(ages[11]);
