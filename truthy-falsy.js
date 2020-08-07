26c7fd6a0b807d43fa73e10933e4b6e8a88952ff
sabirul-islam <sabirul.shimul@gmail.com>
2020-08-07 11:26:46 +0600

    truth vs false

---
+++ truthy-falsy.js
@@ -0,0 +1,21 @@
+// Falsy:
+// false
+// 0
+// ''
+// undefined
+// null
+// NaN
+
+// Truthy:
+// '0'
+// ' '
+// []
+// {}
+
+let name = 12;
+if (name || name == 12){
+    console.log("condition is true");
+}
+else {
+    console.log("condition is false");
+}
